09a6209ac2f922df705e9bede4ae31af4a8997c0
DavidTran <dtrancpp@gmail.com>
2021-03-05 17:42:46 -0800

    Added Galaxy and Universe classes

---
+++ src/Galaxy.java
@@ -0,0 +1,68 @@
+public class Galaxy
+{
+    String type;
+    Boolean interaction;
+    int numOfSolarSystems;
+    int numOfStarSystems;
+    int numOfClusters;
+    int numOfNebulae;
+    int numOfBlackholes;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType( String t ) {
+        t = type;
+    }
+
+    public Boolean getInteraction()
+    {
+        return interaction;
+    }
+
+    public void setInteraction( Boolean i )
+    {
+        i = interaction;
+    }
+
+    public int getNumOfSolarSystems() {
+        return numOfSolarSystems;
+    }
+
+    public void setNumOfSolarSystems( int n ) {
+        n = numOfSolarSystems;
+    }
+
+    public int getNumOfStarSystems() {
+        return numOfStarSystems;
+    }
+
+    public void setNumOfStarSystems( int n ) {
+        n = numOfStarSystems;
+    }
+
+    public int getNumOfClusters() {
+        return numOfClusters;
+    }
+
+    public void setNumOfClusters( int n ) {
+        n = numOfClusters;
+    }
+
+    public int getNumOfNebulae() {
+        return numOfNebulae;
+    }
+
+    public void setNumOfNebulae( int n ) {
+        n = numOfNebulae;
+    }
+
+    public int getNumOfBlackholes() {
+        return numOfBlackholes;
+    }
+
+    public void setNumOfBlackholes( int n ) {
+        n = numOfBlackholes;
+    }
+}
---
+++ src/Universe.java
@@ -0,0 +1,21 @@
+public class Universe
+{
+    double size;
+    int numberOfGalaxies;
+
+    public int getNumberOfGalaxies(){
+        return numberOfGalaxies;
+    }
+
+    public void setNumberOfGalaxies(int numOfGalaxies){
+        numberOfGalaxies = numOfGalaxies;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize( double s) {
+        s = size;
+    }
+}
